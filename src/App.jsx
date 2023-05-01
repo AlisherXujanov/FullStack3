ef71f451972d31a93af95784c6b123fd5ce2f359
Alisher <alisherxujanov163@gmail.com>
2023-05-01 19:53:06 +0500

    last commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Test from './Components/Test'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+
+      <Test />
+    </div>
+  );
+}
+
+export default App;
