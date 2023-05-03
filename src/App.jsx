968dd5556ba40886b7dd0e983bf65dee38726f84
Alisher <alisherxujanov163@gmail.com>
2023-05-03 18:55:01 +0500

    SecondLesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import Test from './Components/Test'
+import SecondLesson from './Components/SecondLesson'
 
 function App() {
+  let people = [
+    { name: 'John', age: 20 },
+    { name: 'Jane', age: 30 },
+    { name: 'Joe', age: 40 },
+    { name: 'Jill', age: 50 },
+    { name: 'Jack', age: 60 }
+  ]
+
   return (
     <div className="App">
       <h1>Hello world</h1>
 
-      <Test />
+      <SecondLesson люди={people} >
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto nulla rem nisi alias minus cum voluptatibus placeat laboriosam reiciendis incidunt. In, accusantium! Possimus iure molestiae, maiores voluptatibus minus distinctio similique?</p>
+      </SecondLesson>
     </div>
   );
 }
---
+++ src/Components/SecondLesson.jsx
@@ -0,0 +1,27 @@
+function SecondLesson(props) {
+    // props.люди   =>  people
+    // let x = ['aaaa', 'bbbb', 'cccc']
+    // let [a, b, c] = x
+
+    // props.children  =>  is used to access the content between the 
+    //                     opening and closing tags of a component
+    // RU: props.children  =>  используется для доступа к содержимому 
+    //                     между открывающим и закрывающим тегами компонента
+
+    let odamlar = props.люди.map((person, index) => {
+        return (
+            <li key={index}>
+                {person.name} - {person.age}
+            </li>
+        )
+    })
+    return (
+        <>
+            <h2>People</h2>
+            { props.children }
+            <ol>{ odamlar }</ol>
+        </>
+    )
+}
+export default SecondLesson
+
