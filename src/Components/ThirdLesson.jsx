359976d672c495fba245d499f920e2b376f12d77
Alisher <alisherxujanov163@gmail.com>
2023-05-05 19:21:07 +0500

    try - catch - finally

--- src/Components/ThirdLesson.jsx
+++ src/Components/ThirdLesson.jsx
@@ -5,11 +5,23 @@ import { useState } from 'react';
 
 function ThirdLesson(props) {
     let [theme, setTheme] = useState('light')
+
+    function toggleTheme() {
+        theme === 'dark' ? setTheme("light") : setTheme("dark")
+        // try {
+        //     throw new Error("Some error")
+        //     theme === 'dark' ? setTheme("light") : setTheme("dark")
+        // } catch (error) {
+        //     console.error(error)
+        // } finally {
+        //     console.log("finally. This is the code that will always run")
+        // }
+    }
     return (
         <>
             <h3>Third Lesson</h3>
-            { theme }
-            <button>
+            {theme}
+            <button onClick={toggleTheme}>
                 Click me!
             </button>
         </>
