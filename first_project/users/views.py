88a334ec2d562208653c72e4fcd4164f1bd682af
Alisher <alisherxujanov163@gmail.com>
2023-09-06 18:37:43 +0500

    migrations

--- first_project/users/views.py
+++ first_project/users/views.py
@@ -3,7 +3,4 @@ from django.shortcuts import render
 
 
 def first_view(request):
-    return HttpResponse("""
-            <h1 style="color:red;">Hello, World!</h1>
-            <b>Hello, World!</b>
-        """)
+    return render(request, 'index.html')
