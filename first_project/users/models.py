91e0b2c9f5e7a3bea477ea9b87bc3362b27e1be1
Alisher <alisherxujanov163@gmail.com>
2023-09-06 18:53:46 +0500

    admin.py

--- first_project/users/models.py
+++ first_project/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+
+# python manage.py makemigrations users
