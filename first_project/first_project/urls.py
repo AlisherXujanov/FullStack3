e3ef30acad3c8830b229f26712bf1e53ad6d8fec
Alisher <alisherxujanov163@gmail.com>
2023-09-08 19:41:40 +0500

    home page

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import home
+
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
 ]
