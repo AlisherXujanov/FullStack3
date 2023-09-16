d2c8d7eb3d326931b55d92ac9bee4e3440f747b6
Alisher <alisherxujanov163@gmail.com>
2023-09-16 11:10:42 +0500

    small changes and form

--- first_project/users/forms.py
+++ first_project/users/forms.py
@@ -4,11 +4,10 @@ from .models import User
 
 
 class UserForm(forms.ModelForm):
-    first_name = forms.CharField(
-        max_length=255, label="Your name",
-        widget=forms.PasswordInput(attrs={'placeholder': 'Your name'})
-    )
-    last_name = forms.CharField(max_length=255, label="Your surname")
+    first_name = forms.CharField(max_length=255, label="First name",
+                                 help_text="Please enter your name")
+    last_name = forms.CharField(max_length=255, label="Last name",
+                                help_text="Please enter your last name")
     email = forms.EmailField(required=True, label="Your email",
                              help_text="We will never share your information with anyone!")
     image = forms.ImageField(required=False, label="Your image")
