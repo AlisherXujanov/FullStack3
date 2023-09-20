ad1067094aa8f1ccdedf19c957e30356e71385be
Alisher <alisherxujanov163@gmail.com>
2023-09-20 18:57:15 +0500

    UserForm

---
+++ first_project/users/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class UserForm(UserCreationForm):
+    email = forms.EmailField(required=True,
+                             help_text="We never share your email with anyone else.")
+
+    class Meta:
+        model = User
+        fields = ('username', 'email',
+                  'password1', 'password2')
