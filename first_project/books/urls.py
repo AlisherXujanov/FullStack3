67bec626bf695026d5c400792712306217f51347
Alisher <alisherxujanov163@gmail.com>
2023-09-25 19:06:07 +0500

    CreateView

--- first_project/books/urls.py
+++ first_project/books/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path("", BooksListView.as_view(), name="books_view"),
-    path("add_book", add_book, name="add_book"),
+    path("add_book", AddBookView.as_view(), name="add_book"),
     path("book_details/<int:pk>", BookDetailsView.as_view(), name="book_details"),
     # path("details/<int:book_id>", book_details, name="book_details"),
     # path("delete/<int:del_id>", delete_book, name="delete_book")
--- first_project/books/views.py
+++ first_project/books/views.py
@@ -3,26 +3,32 @@ from typing import Any
 from django.contrib import messages
 from django.db import models
 from django.shortcuts import redirect, render
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView
 
 from .forms import BookForm
 from .models import Books
 
+# def add_book(request):
+#     if request.method == 'POST':
+#         form = BookForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             book = form.save(commit=False)
+#             # book.author = request.user
+#             book.save()
+#             messages.success(request, 'Book added successfully!')
+#             return redirect('books_view')
+#     else:
+#         form = BookForm()
 
-def add_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST, request.FILES)
-        if form.is_valid():
-            book = form.save(commit=False)
-            # book.author = request.user
-            book.save()
-            messages.success(request, 'Book added successfully!')
-            return redirect('books_view')
-    else:
-        form = BookForm()
+#     context = {'form': form}
+#     return render(request, 'add_book.html', context)
 
-    context = {'form': form}
-    return render(request, 'add_book.html', context)
+
+class AddBookView(CreateView):
+    modal = Books
+    form_class = BookForm
+    template_name = 'add_book.html'
+    success_url = '/books/'
 
 
 # def books(request):
